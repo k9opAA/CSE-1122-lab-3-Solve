193c09d53a8802e630c70dc3f98e446c0595c38a
Ali Azgor <140571734+k9opAA@users.noreply.github.com>
2024-05-23 21:27:12 +0600

    Update 7.c

--- 7.c
+++ 7.c
@@ -1,30 +1,21 @@
 Write a program to calculate the factorial of a positive integer quantity (using recursion).
 
 #include <stdio.h>
-
-// Recursive function to calculate factorial
-unsigned long long factorial(int n) {
-    if (n == 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+int factorial(int n) {
+ if (n == 0) {
+ return 1;
+ } else {
+ return n*factorial(n-1);
+ }
 }
-
 int main() {
-    int number;
-
-    // Prompt the user for input
-    printf("Enter a positive integer: ");
-    scanf("%d", &number);
-
-    // Ensure the number is positive
-    if (number < 0) {
-        printf("Factorial is not defined for negative numbers.\n");
-    } else {
-        unsigned long long fact = factorial(number);
-        printf("Factorial of %d is %llu\n", number, fact);
-    }
-
-    return 0;
+ int num,fact;
+ scanf("%d",&num);
+ if(num<0) {
+ } else {
+ fact=factorial(num);
+ printf("Factorial of %d is = %d\n",num,fact);
+ }
+ return 0;
+}
 }
